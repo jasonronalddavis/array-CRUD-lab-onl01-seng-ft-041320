7cd888e3936f2953153a40927633fcdf4bdbdbb4
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 16:00:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   barns = ["chicken", "pig", "cow", "horse"]
-  barns.push("goat")
+  barns.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
