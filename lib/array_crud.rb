ff6f076df34c857d0adce7de285d9058dc8bf08d
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 16:42:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,17 +8,17 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  element.push("arrays!")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning", "arrays!"]
-  element.unshift("wow")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element.pop("arrays!")
+  array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
