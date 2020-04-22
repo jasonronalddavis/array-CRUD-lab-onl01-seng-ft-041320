3f9adf03dbe09c892d1cc2ee689a4b32a6e513bf
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 16:10:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  barns = ["chicken", "pig", "cow", "horse"]
+  barns = ["wow", "chicken", "pig", "cow", "horse","arrays!"]
   barns.pop("arrays!")
 end
 
