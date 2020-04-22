4982b322d7cebd080a0db6be322f37419c298120
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 16:33:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,8 @@ def add_element_to_start_of_array(array, element)
   barns.unshift("wow")
 end
 
-def remove_element_from_end_of_array(barnes)
+def remove_element_from_end_of_array(array)
+barns = ["wow", "I", "am", "really", "learning", "arrays!"]
   barns.pop("arrays!")
 end
 
